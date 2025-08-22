8893fa2e0cd0554e7edd302ecc58ab93fc840415
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2025-08-23 00:53:37 +0200

    Revert "[Java] Add debug trace."
    This reverts commit 593affc9e995a9eeaaba9bbf192eefaa4b4d7991.

--- aeron-system-tests/src/test/java/io/aeron/archive/ArchiveReplayTest.java
+++ aeron-system-tests/src/test/java/io/aeron/archive/ArchiveReplayTest.java
@@ -46,10 +46,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.io.File;
-import java.time.Instant;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Supplier;
 
 import static io.aeron.CommonContext.IPC_CHANNEL;
 import static io.aeron.archive.client.AeronArchive.NULL_POSITION;
@@ -175,18 +173,11 @@ public class ArchiveReplayTest
             final Subscription replay = aeron.addSubscription(
                 replayChannel, replayStreamId, availableImage::set, unavailableImage::set);
 
-            System.out.println("*** " + java.time.Instant.now() +
-                " Replay subscription created: clientId=" + aeron.clientId() +
-                ", subscriptionRegistrationId=" + replay.registrationId());
-
-            final Supplier<String> messageSupplier =
-                () -> "*** " + Instant.now() + " no Image for subscriptionRegistrationId=" +
-                replay.registrationId();
             Image image;
             while (null == (image = availableImage.get()))
             {
                 aeronArchive.checkForErrorResponse();
-                Tests.yieldingIdle(messageSupplier);
+                Tests.yield();
             }
 
             while (!image.isEndOfStream())
