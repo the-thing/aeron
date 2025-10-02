8bfec8fa7a576ecb2c08c2fdffc0ee37f3bf18d5
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2025-10-02 13:19:18 +0200

    [C] Upgrade to `CMake` 4.1.2.

--- cppbuild/cppbuild.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-
-echo Building Aeron using the Window Batch script is no longer supported, use a PowerShell terminal and run cppbuild\cppbuild.ps1
--- cppbuild/cppbuild.ps1
+++ cppbuild/cppbuild.ps1
@@ -155,7 +155,7 @@ for ($i = 0; $i -lt $Args.count; $i++)
 
 $BuildDir = "$PSScriptRoot\$BuildConfig"
 $SourceDir = "$PSScriptRoot\.."
-$CMakeVersion = "3.30.0"
+$CMakeVersion = "4.1.2"
 $CMakeDirName = "cmake-$CMakeVersion-windows-x86_64"
 $CMakeArchive = "$CMakeDirName.zip"
 $CMakePath = "$PSScriptRoot\$CMakeDirName"
