6fc252e5b1453be823f2875454798104b19e6391
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2026-03-18 22:49:01 +0100

    [C++ Wrapper] Manually free `AsyncAddSubscription` in case an attempt to close the underlying `aeron_subscription_t` fails with the `AERON_CLIENT_ERROR_BUFFER_FULL` error, i.e. if the client command buffer is full in which case `aeron_subscription_t` will not be closed explicitly.
    In this case the `aeron_subscription_t` will remain active until `aeron_t` is closed. However, when `aeron_t` is closed it invokes `aeron_subscription_close` without any callbacks. As a result the `AsyncAddSubscription::remove` won't be invoked thus leading to a memory leak. Hence, we manually free `AsyncAddSubscription`.

--- aeron-client/src/main/cpp_wrapper/Subscription.h
+++ aeron-client/src/main/cpp_wrapper/Subscription.h
@@ -104,7 +104,16 @@ public:
 
     ~Subscription()
     {
-        aeron_subscription_close(m_subscription, AsyncAddSubscription::remove, m_addSubscription);
+        if (0 != aeron_subscription_close(m_subscription, AsyncAddSubscription::remove, m_addSubscription))
+        {
+            // failed to submit close request: the underlying `aeron_subscription_t` will be eventually closed when
+            // `aeron_t` is closed but the `AsyncAddSubscription` needs to be manually freed as no `on_close_complete`
+            // callback will be invoked in this case.
+            if (aeron_errcode() == AERON_CLIENT_ERROR_BUFFER_FULL)
+            {
+                AsyncAddSubscription::remove(m_addSubscription);
+            }
+        }
     }
 
     /**
