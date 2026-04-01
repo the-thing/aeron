3998d34f7a7af70529abb17e14fb22c90f0e65cd
Peter Veentjer <peter.veentjer@weareadaptive.com>
2026-04-01 11:48:58 +0300

    c-driver: socket can leak with multicast. (#1973)
    If multicast is enabled and connect_addr != null, then a second
    socket is opened (the receive socket).
    
    At this point 2 sockets are open.
    
    If something else fails after the second socket is opened,
    then there is a jump to the error handler. The error handler closes
    only the first socket, but not the second.
    
    And you end up with a leaked socket.

--- aeron-driver/src/main/c/media/aeron_udp_channel_transport.c
+++ aeron-driver/src/main/c/media/aeron_udp_channel_transport.c
@@ -317,12 +317,18 @@ int aeron_udp_channel_transport_init(
     return 0;
 
 error:
+    if (-1 != transport->recv_fd && transport->recv_fd != transport->fd)
+    {
+        aeron_close_socket(transport->recv_fd);
+    }
+
     if (-1 != transport->fd)
     {
         aeron_close_socket(transport->fd);
     }
 
     transport->fd = -1;
+    transport->recv_fd = -1;
     return -1;
 }
 
